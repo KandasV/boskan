d09871f46d0f958300fee5db6481f4c3ee53dbc5
Ozan Kandas <36104826+KandasV@users.noreply.github.com>
2022-12-23 23:57:42 +0300

    Add files via upload

---
+++ production/pc_parts/page_layouts/topbar.php
@@ -0,0 +1,19 @@
+<?php
+
+function topNavBar(){
+	
+	$topbar = '
+        <div class="top_nav">
+            <div class="nav_menu">
+                <ul class="navbar">
+                    <img src="images/logo_white.png" width="190px" height="50px">
+                </ul>
+            </nav>
+            </div>
+        </div>
+	';
+	
+	echo $topbar;	
+}
+
+?>
---
+++ production/pc_parts/page_layouts/urunStyle.php
@@ -0,0 +1,18 @@
+<?php
+
+function urunStyle(){
+	
+	$urunStyle = '
+      <style>
+          .main_container{background: #F5F7FA;}
+          .site_title{color:black;}
+          body{background: #F5F7FA;}
+          .nav-md .container.body .right_col { margin-left: 120px; }
+          .nav_menu{background:#2A3F54 ;}
+    </style>
+	';
+	
+	echo $urunStyle;	
+}
+
+?>
